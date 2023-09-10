2386262514c546d19949866f09b0e86a16dc0bed
RawZ06 <44646690+RawZ06@users.noreply.github.com>
2023-09-11 00:07:56 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'primereact/button';
+
+export default function App() {
+    return (
+        <div>
+            <div className="underline">Hello</div>
+            <Button label="Check" icon="pi pi-check" />
+        </div>
+    );
+}
